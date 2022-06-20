1e69902a59b3e9fd3bd9a088389113adae039dfa
hudahai <hscxrzs@gmail.com>
2022-06-20 22:06:59 +0800

    feat: generate parenthesis

---
+++ src/main/java/leetcode/GenerateParenthesis.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+    List<String> result;
+
+    public GenerateParenthesis() {
+        this.result = new ArrayList<>();
+    }
+
+    public void recur(String cur, int c1, int c2) {
+        if (c1 == 0 && c2 == 0) {
+            this.result.add(cur);
+            return;
+        }
+        if (c1 == c2) {
+            this.recur(cur + "(", c1 - 1, c2);
+        } else {
+            // c1 < c2
+            if (c1 > 0) {
+                this.recur(cur + "(", c1 - 1, c2);
+            }
+            this.recur(cur + ")", c1, c2 - 1);
+        }
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+        this.recur("", n, n);
+        return this.result;
+    }
+}
---
+++ src/test/java/leetcode/TestGenerateParenthesis.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class TestGenerateParenthesis {
+    @Test
+    public void testGenerateParenthesis() {
+        System.out.println(new GenerateParenthesis().generateParenthesis(3));
+    }
+}
